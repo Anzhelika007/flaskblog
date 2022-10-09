25c02128c86044a11555d4871f446114b40fc6aa
Anzhalika Turlak <turlak.by@gmail.com>
2022-10-09 21:10:35 +0300

    add template call home task

--- app.py
+++ app.py
@@ -2,16 +2,32 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
+@app.route('/home')
 def index():
-    return('Hello world')
+    return 'Hello world'
 
 
 @app.route('/about')
 def about():
-    return('About as')
+    return 'About as'
+
+
+@app.route('/info')
+def info():
+    return 'Read me'
+
+
+@app.route('/catalog')
+def catalog():
+    return 'Сhoose a product'
+
+
+@app.route('/user/<string:name>/<int:id>')
+def user(name, id):
+    return 'User page' + name + '-' + id
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
